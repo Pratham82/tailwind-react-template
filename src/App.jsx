6039a8b1cf5822e5312796ba37a3123369a4a60e
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-18 23:59:20 +0530

    feat: added tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className="bg-slate-100 min-h-screen text-center">
+      <Navbar />
+      <Card />
+    </div>
+  )
+}
