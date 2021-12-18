520e7014a7cf2caa79c0a25f04ba2d68f46fc0ee
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-19 01:59:34 +0530

    feat: added user card list in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Card from './components/Card'
+import CardList from './components/CardList'
 import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <div className="bg-slate-100 min-h-screen text-center">
       <Navbar />
-      <Card />
+      <CardList />
     </div>
   )
 }
