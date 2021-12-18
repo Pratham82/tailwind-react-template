82afa00606a398cb5380f38a1c13492bdc233415
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-19 01:58:41 +0530

    refactor: updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 export default function Navbar() {
   const navContents = ['Home', 'About', 'Contacts']
   return (
-    <nav className="bg-slate-700 flex text-white justify-between items-center py-3 px-10 m-2 rounded-lg">
+    <nav className="bg-slate-700 flex text-white justify-between items-center py-3 px-10">
       <h1 className="text-xl">Brand</h1>
       <ul className="flex">
-        {navContents.map(el => (
-          <li className="pr-5 text-sm">{el}</li>
+        {navContents.map((el, i) => (
+          <li key={i} className="pr-5 text-sm">
+            {el}
+          </li>
         ))}
       </ul>
     </nav>
