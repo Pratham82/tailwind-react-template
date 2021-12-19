d229d11ca71d29fc222b154539b77dabd33863b3
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-20 00:05:37 +0530

    fix: removed unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import FlexLayout from './components/FlexLayout'
-import GridLayout from './components/GridLayout'
 import CardList from './components/CardList'
 import Navbar from './components/Navbar'
-import Carousel from './components/Carousel'
 
 export default function App() {
   return (
-    <div className="bg-slate-100 min-h-screen text-center ">
+    <div className="bg-slate-100 min-h-screen text-center">
       <Navbar />
       <CardList />
-      {/* <FlexLayout /> */}
-      {/* <GridLayout /> */}
-      {/* <Carousel /> */}
     </div>
   )
 }
