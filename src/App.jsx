e404ece060df23a594fe1f9a726188b22c26db42
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-21 23:54:36 +0530

    feat: added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProfileDetails from './components/ProfileDetails'
 import CardList from './components/CardList'
 import Navbar from './components/Navbar'
+import PageNotFound from './pages/404'
 
 export default function App() {
   return (
     <div className="bg-slate-100 min-h-screen text-center">
       <Navbar />
-      <CardList />
+      <Router>
+        <Routes>
+          <Route path="/" element={<CardList />} />
+          <Route path="/profile" element={<ProfileDetails />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
