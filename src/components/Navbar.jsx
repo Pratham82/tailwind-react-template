57d939f28b9d94973d2c27f10ef7e195f539e6d9
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-22 00:52:28 +0530

    feat: added links in navbar using react router hooks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
+import { useNavigate } from 'react-router-dom'
+
 export default function Navbar() {
-  const navContents = ['Home', 'About', 'Contacts']
+  const navigate = useNavigate()
+  const navContents = [
+    { title: 'Users list', path: '/' },
+    { title: 'About', path: '/about' },
+    { title: 'Contacts', path: '/contacts' },
+  ]
   return (
-    <nav className="bg-slate-700 flex text-white justify-between items-center py-3 px-10">
-      <h1 className="text-xl">Brand</h1>
+    <nav className="bg-slate-700 flex text-white justify-around items-center py-3 px-10">
+      <h1 className="text-xl cursor-pointer" onClick={() => navigate('/')}>
+        Tailwind x React
+      </h1>
       <ul className="flex">
         {navContents.map((el, i) => (
-          <li key={i} className="pr-5 text-sm">
-            {el}
+          <li
+            onClick={() => navigate(`${el.path}`)}
+            key={i}
+            className="pr-5 text-sm cursor-pointer"
+          >
+            {el.title}
           </li>
         ))}
       </ul>
