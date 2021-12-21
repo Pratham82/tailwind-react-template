60f38a16f6841cdaef1f731d9ac85b265c328ba5
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-22 00:50:32 +0530

    feat: added 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+export default function PageNotFound() {
+  return (
+    <div className="grid place-content-center h-96">
+      <h1 className="text-3xl text-slate-900 font-extrabold">
+        404 Page Not found !!
+      </h1>
+    </div>
+  )
+}
