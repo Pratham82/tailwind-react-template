e816e7e189c1f0830054abb62d967e9f9bbcc425
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-22 00:53:25 +0530

    refactor: changed to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 export default function Button({ text }) {
   return (
-    <div
+    <button
       className={`h-10 w-30 bg-blue-500/90 p-5 m-5 grid place-content-center text-white text-sm shadow-lg shadow-blue-500/60 hover:bg-blue-500/80 hover:scale-95 hover:shadow-blue-500/20 transition ease-in cursor-pointer rounded-lg`}
     >
       {text}
-    </div>
+    </button>
   )
 }
