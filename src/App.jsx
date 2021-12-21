acba118fe0cc8890e40ef72505822cf40b4606d7
Prathamesh Mali <mali.prathamesh82@gmail.com>
2021-12-22 00:53:48 +0530

    refactor: added new router

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import ProfileDetails from './components/ProfileDetails'
 import CardList from './components/CardList'
 import Navbar from './components/Navbar'
 import PageNotFound from './pages/404'
+import About from './components/About'
 
 export default function App() {
   return (
     <div className="bg-slate-100 min-h-screen text-center">
-      <Navbar />
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<CardList />} />
           <Route path="/profile" element={<ProfileDetails />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
